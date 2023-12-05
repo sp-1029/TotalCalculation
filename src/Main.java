4c519e36c3f3f0a891ed68c1cb5e999b3583ad01
sp-1029 <jaj666420@gmail.com>
2023-12-04 17:48:26 -0700

    Started framework for TotalCalculation

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("items.txt");
-        try (FileWriter fileWriter = new FileWriter(file, true)) {
-            fileWriter.write("woah I really work\n");
+        double subtotal = 0;
+        File items = new File("items.txt");
+        File totalFile = new File("total.txt");
+        try {
+            Scanner reader = new Scanner(items);
+            while (reader.hasNextDouble()) {
+                subtotal += reader.nextDouble();
+            }
         } catch (IOException e) {
-            System.out.println("Damn you schitzo that file doesn't exist");
+            throw new RuntimeException(e);
         }
-
-        try (Scanner scanner = new Scanner(new BufferedInputStream(new FileInputStream(file)))) {
-            System.out.println(scanner.nextLine());
+        subtotal *= 100;
+        double tax = (subtotal * 0.053);
+        double totalMoney = (subtotal + tax);
+        subtotal = (double) Math.round(subtotal) / 100;
+        tax = (double) Math.round(tax) / 100;
+        totalMoney = (double) Math.round(totalMoney) / 100;
+        try {
+            FileWriter fw = new FileWriter(totalFile);
+            fw.write("The sub-total is $" + subtotal + '\n' + "The tax is $" + tax + '\n' + "The total is $" + totalMoney);
+            fw.close();
         } catch (IOException e) {
-
+            throw new RuntimeException(e);
         }
     }
 }
